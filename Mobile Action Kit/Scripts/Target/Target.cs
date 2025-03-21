69867cb7a2e4f04c25d3937b53cdf5bd0bef363f
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 19:14:30 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Scripts/Target/Target.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MobileActionKit
+{
+    public class Target : MonoBehaviour
+    {
+        [TextArea]
+        [ContextMenuItem("Reset Description", "ResettingDescription")]
+        public string ScriptInfo = "This Script Rotates The X Axis of Target If Shoot By Any Weapon";
+        [Space(10)]
+
+        public bool EnableTargetHitRotation = false;
+        public float XRotationSpeed;
+        public bool DeactivateOnRotationComplete = true;
+
+        [HideInInspector]
+        public bool StartRotating = false;
+
+        public void ResettingDescription()
+        {
+            ScriptInfo = "This Script Rotates The X Axis of Target If Shoot By Any Weapon";
+        }
+        void Update()
+        {
+            if(EnableTargetHitRotation == true)
+            {
+                if (StartRotating == true)
+                {
+                    if (transform.rotation.x >= 0f)
+                    {
+                        if(DeactivateOnRotationComplete == true)
+                        {
+                            gameObject.SetActive(false);
+                        }
+                    }
+                    else
+                    {
+                        transform.Rotate(XRotationSpeed * Time.deltaTime, transform.rotation.y, transform.rotation.z);
+                    }
+                }
+            }
+        }
+        public void Damage()
+        {
+            StartRotating = true;
+        }
+
+
+
+    }
+}
