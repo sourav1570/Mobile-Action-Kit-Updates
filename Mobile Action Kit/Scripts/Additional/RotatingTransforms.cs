e5b0c762ef2e7a0f9ad9a1c04c9f07285be61b0c
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 19:02:42 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Scripts/Additional/RotatingTransforms.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MobileActionKit
+{
+    public class RotatingTransforms : MonoBehaviour
+    {
+
+        public static void ChangeRotation(Transform ThisTransform, Vector3 From, Vector3 to, float speed)
+        {
+            var newRotation = Quaternion.Slerp(ThisTransform.rotation, Quaternion.LookRotation(From - to), speed * Time.deltaTime).eulerAngles;
+            newRotation.x = 0;
+            newRotation.z = 0;
+            ThisTransform.rotation = Quaternion.Euler(newRotation);
+        }
+
+    }
+}
