b912b5f6e2df866237782950925633073bd78850
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 19:07:19 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Scripts/Helper Scripts/DeveloperDebugger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+// Script For Debugging Number of Enemies Alive !
+
+namespace MobileActionKit
+{
+    public class DeveloperDebugger : MonoBehaviour
+    {
+
+        public bool ShouldDebug = true;
+        public Text TextToDebugAliveEnemies;
+
+        void Update()
+        {
+            if (ShouldDebug == true)
+            {
+                GameObject[] Enemies = GameObject.FindGameObjectsWithTag("Enemy");
+                int Myenemy = Enemies.Length;
+                TextToDebugAliveEnemies.text = "CURRENT" + " " + "ENEMIES" + " " + "-" + Myenemy.ToString();
+            }
+        }
+    }
+}
