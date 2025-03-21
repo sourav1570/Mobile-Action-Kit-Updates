4dd5041c4d6e1f46baaea1fdb66dd7d5540920f2
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 19:01:59 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Scripts/Additional/DeactivateOnPlay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MobileActionKit
+{
+	public class DeactivateOnPlay : MonoBehaviour
+	{
+
+		public MeshRenderer[] AddMeshRenderers;
+		public SkinnedMeshRenderer[] AddSkinMeshRenderers;
+		public GameObject[] AddGameObjects;
+
+		void Start()
+		{
+			for (int x = 0; x < AddMeshRenderers.Length; x++)
+			{
+				AddMeshRenderers[x].enabled = false;
+			}
+			for (int x = 0; x < AddSkinMeshRenderers.Length; x++)
+			{
+				AddSkinMeshRenderers[x].enabled = false;
+			}
+			for (int x = 0; x < AddGameObjects.Length; x++)
+			{
+				AddGameObjects[x].SetActive(false);
+			}
+		}
+
+	}
+}
