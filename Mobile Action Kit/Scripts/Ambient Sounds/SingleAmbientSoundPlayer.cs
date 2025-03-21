e6143eb353cfe704bf836a6fa923f7600c19d811
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 19:05:59 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Scripts/Ambient Sounds/SingleAmbientSoundPlayer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace MobileActionKit
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SingleAmbientSoundPlayer : MonoBehaviour
+    {
+        public AudioClip AudioClipToPlay;
+        public float AudioVolume;
+        public bool ShouldLoop = true;
+        [Range(0, 256)]
+        public int AudioPriority = 128;
+
+        private AudioSource CurrentAudioSource;
+
+        public void Start()
+        {
+            CurrentAudioSource = GetComponent<AudioSource>();
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.transform.root.tag == "Player")
+            {
+                CurrentAudioSource.clip = AudioClipToPlay;
+                CurrentAudioSource.volume = AudioVolume;
+                CurrentAudioSource.loop = ShouldLoop;
+                CurrentAudioSource.priority = AudioPriority;
+                CurrentAudioSource.Play();
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.transform.root.tag == "Player")
+            {
+                CurrentAudioSource.Stop();
+            }
+        }
+    }
+}
