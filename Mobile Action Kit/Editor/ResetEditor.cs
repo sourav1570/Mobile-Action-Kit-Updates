926439a0989ea79c8dc06f6ca4cb3443248f5bc0
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 18:51:37 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Editor/ResetEditor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+namespace MobileActionKit
+{
+    [CustomEditor(typeof(DeveloperMode))]
+    public class ResetEditor : Editor
+    {
+        public SerializedProperty ScriptInfo;
+        public SerializedProperty AddGoldBars;
+        public SerializedProperty GoldBars;      
+        DeveloperMode st;
+        void OnEnable()
+        {
+            // Setup the SerializedProperties
+            AddGoldBars = serializedObject.FindProperty("AddGoldBars");
+            GoldBars = serializedObject.FindProperty("GoldBars");
+            ScriptInfo = serializedObject.FindProperty("ScriptInfo");
+            st = target as DeveloperMode;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            //DrawDefaultInspector();
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(ScriptInfo);
+            EditorGUILayout.PropertyField(AddGoldBars);
+
+            if (st.AddGoldBars == true)
+            {
+                EditorGUILayout.PropertyField(GoldBars);
+            }
+
+            DeveloperMode datascript = (DeveloperMode)target;
+            if (GUILayout.Button("Delete Saved Data Stored"))
+            {
+                datascript.deletealldata();
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+    }
+}
