e27388a58009a4e1738b14cfcef973ebf66c72c6
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 19:09:05 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Scripts/Player Gameplay/HealthBarScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+namespace MobileActionKit
+{
+    public class HealthBarScript : MonoBehaviour
+    {
+        [TextArea]
+        [ContextMenuItem("Reset Description", "ResettingDescription")]
+        public string ScriptInfo = "This Script Reduces The Player UI Health Bar in game";
+        [Space(10)]
+
+        private float fillamount = 1;
+        [SerializeField]
+        private float HealthBarSpeed;
+        [SerializeField]
+        private Image HealthBarImage;
+
+        public float Maxvalue { get; set; }
+
+        public void ResettingDescription()
+        {
+            ScriptInfo = "This Script Reduces The Player UI Health Bar in game";
+        }
+        public float Value
+        {
+            set
+            {
+                fillamount = Map(value, 0, Maxvalue, 0, 1);
+            }
+        }
+        void Update()
+        {
+            Handleabar();
+        }
+        private void Handleabar()
+        {
+            if (fillamount != HealthBarImage.fillAmount)
+            {
+                HealthBarImage.fillAmount = Mathf.Lerp(HealthBarImage.fillAmount, fillamount, Time.deltaTime * HealthBarSpeed);
+            }
+        }
+        private float Map(float value, float inmin, float inmax, float outmin, float outmax)
+        {
+            return (value - inmin) * (outmax - outmin) / (inmax - inmin) + outmin;
+        }
+    }
+}
