55c8156f505510e8385e19701f0584be575d20a0
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 18:51:06 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Editor/PlayerEditorScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace MobileActionKit
+{
+    public class PlayerEditorScript : Editor
+    {
+        [MenuItem("Tools/Mobile Action Kit/Player")]
+        public static void PlayerEditorSetup()
+        {
+            
+        }
+    }
+}
