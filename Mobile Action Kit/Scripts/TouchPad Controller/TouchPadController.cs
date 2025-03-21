74eb4a079bb7c60cdb0374d99839ae445a74b1f8
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 19:14:40 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Scripts/TouchPad Controller/TouchPadController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace MobileActionKit
+{
+    public class TouchPadController : MonoBehaviour
+    {
+
+        [TextArea]
+        [ContextMenuItem("Reset Description", "ResettingDescription")]
+        public string ScriptInfo = "This Script Updates The Camera X and Y Rotation Every Frame";
+        [Space(10)]
+        public FirstPersonController FirstPersonControllerScript;
+        public TouchPad TouchPadScript;
+        public void ResettingDescription()
+        {
+            ScriptInfo = "This Script Updates The Camera X and Y Rotation Every Frame";
+        }
+        void Update()
+        {
+            FirstPersonControllerScript.TouchpadLook.LookAxis = TouchPadScript.TouchDistance;
+        }
+    }
+}
