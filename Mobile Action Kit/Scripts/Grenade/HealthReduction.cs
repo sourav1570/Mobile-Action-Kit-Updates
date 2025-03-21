8686584159da0fe1cd07a4d28534ad2493fcfdc6
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 19:06:40 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Scripts/Grenade/HealthReduction.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MobileActionKit
+{
+    public class HealthReduction : MonoBehaviour
+    {
+        public float HealthReductionDuration = 3f;
+
+        public Color MaterialColorToChange = Color.grey;
+
+    }
+}
