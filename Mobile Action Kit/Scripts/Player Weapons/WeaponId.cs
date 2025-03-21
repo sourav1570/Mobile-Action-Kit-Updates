2fda82758d0783cc43d6326e5950ddf0972e7936
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 19:12:14 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Scripts/Player Weapons/WeaponId.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MobileActionKit
+{
+    public class WeaponId : MonoBehaviour
+    {
+        [TextArea]
+        public string ScriptInfo = "This Script Store and Save The Weapon Information using the Unique Weapon Name ( Provided Below )";
+        [Space(10)]
+
+        [Tooltip("A Unique Name For Each Weapon To Save The Game Data * Weapon Names Cannot Be Similar")]
+        public string WeaponName;
+
+    }
+}
