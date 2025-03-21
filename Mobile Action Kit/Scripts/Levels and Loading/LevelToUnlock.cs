a5843776e068f75e3b9df2ec044a9539719ea887
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 19:07:55 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Scripts/Levels and Loading/LevelToUnlock.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace MobileActionKit
+{
+    public class LevelToUnlock : MonoBehaviour
+    {
+        [TextArea]
+        public string ScriptInfo = "This script manages level unlocking. When a player completes a level, it updates the highest level reached in PlayerPrefs, allowing progression to the next level.";
+
+        public static LevelToUnlock instance;
+
+        [Tooltip("The next level that should be unlocked upon completion of the current level.")]
+        public int NextLevelToUnlock = 1;
+
+        int LevelReached;
+
+        #region Singleton
+        void Awake()
+        {
+            instance = this;
+        }
+        #endregion
+
+        void Start()
+        {
+            LevelReached = PlayerPrefs.GetInt("LevelReached");
+        }
+        public void LevelCompleted()
+        {
+            if (LevelReached < NextLevelToUnlock)
+            {
+                PlayerPrefs.SetInt("LevelReached", NextLevelToUnlock);
+            }
+        }
+    }
+}
