9ce034695267fc9f2b5e204a5a0308d50c66ad66
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 19:07:22 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Scripts/Helper Scripts/DeveloperMode.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace MobileActionKit
+{
+    public class DeveloperMode : MonoBehaviour
+    {
+        [TextArea]
+        [ContextMenuItem("Reset Description", "ResettingDescription")]
+        public string ScriptInfo = "This Script can be useful for developers testing the game or kit ";
+        [Space(10)]
+
+        [Tooltip("Add cash in the game for testing.")]
+        public bool AddGoldBars;
+        [Tooltip("Enter the amount to add.")]
+        public int GoldBars;
+
+        public void ResettingDescription()
+        {
+            ScriptInfo = "This Script can be useful for developers testing the game or kit ";
+        }
+        private void Start()
+        {
+            if (AddGoldBars == true)
+            {
+                PlayerPrefs.SetInt("ShopMoney", GoldBars);
+                if(GameCurrencyManager.instance != null)
+                {
+                    GameCurrencyManager.instance.TotalGoldText.text = PlayerPrefs.GetInt("ShopMoney").ToString();
+                }
+            }
+        }
+        public void deletealldata()
+        {
+            PlayerPrefs.DeleteAll();
+           
+        }
+    }
+}
