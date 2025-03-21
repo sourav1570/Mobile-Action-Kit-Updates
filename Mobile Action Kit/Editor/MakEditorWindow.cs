89bfb47709ab5b157dd0d532be7bc405678c9de8
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 18:51:02 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Editor/MakEditorWindow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace MobileActionKit
+{
+    public class MakEditorWindow : EditorWindow
+    {
+
+        //[MenuItem("Tools/MobileActionKit/About")]
+        //public static void ShowWindow()
+        //{
+        //    GetWindow<MakEditorWindow>("MAK").Show();
+        //    GetWindow<MakEditorWindow>("MAK");
+        //}
+        //void OnGUI()
+        //{
+        //    GUILayout.Label("                                  What's New", EditorStyles.boldLabel);
+        //    EditorGUILayout.Space();
+        //    GUILayout.Label("1.Added Helicopter");
+        //    GUILayout.Label("2.Minor Bugs Fixed");
+        //    GUILayout.Label("3.Added New Functionality");
+        //    GUILayout.Label("4.Improvement in AI");
+        //    GUILayout.Label("5.Added AirCrafts");
+        //}
+
+    }
+}
