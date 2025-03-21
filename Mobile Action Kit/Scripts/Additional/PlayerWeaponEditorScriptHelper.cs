1cb9226ad8d96c71796bc7fa378e993f1e7e6afd
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 19:02:31 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Scripts/Additional/PlayerWeaponEditorScriptHelper.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MobileActionKit
+{
+    public class PlayerWeaponEditorScriptHelper : MonoBehaviour
+    {
+        public PlayerWeapon PlayerWeaponScript;
+        public GameObject[] Sounds;
+    }
+}
