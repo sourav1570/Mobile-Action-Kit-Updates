8facfc8ee973091b6988646c39df58de26b70ad6
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 18:57:03 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Art/FpsCounter/RotateClock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This Script is Responsible For Different Settings in Game
+namespace MobileActionKit
+{
+
+	public class RotateClock : MonoBehaviour
+	{
+		void Update()
+		{
+			transform.Rotate(0, 0, -6);
+		}
+	}
+}
