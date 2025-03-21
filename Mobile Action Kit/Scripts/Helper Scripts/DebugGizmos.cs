d2486633483c2393e8581e2a4f14e7113ac5a6f5
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-03-21 19:07:11 +0530

    Updating files to version 1.0.2

---
+++ Mobile Action Kit/Scripts/Helper Scripts/DebugGizmos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MobileActionKit
+{
+    public class DebugGizmos : MonoBehaviour
+    {
+        public float Radius;
+
+        void OnDrawGizmos()
+        {
+            // Display the explosion radius when selected
+            Gizmos.color = new Color(1, 1, 0, 0.75F);
+            Gizmos.DrawSphere(transform.position, Radius);
+        }
+
+
+    }
+}
